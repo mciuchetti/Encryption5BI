c9ad1ced0167438b83ad046ee6ee557a617baedc
Monica Ciuchetti <35596533+mciuchetti@users.noreply.github.com>
2023-12-04 21:56:39 +0100

    Lettura parola chiave

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String parolaChiave;
+    System.out.println("Inserisci la parola chiave:");
+    try{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      parolaChiave = br.readLine();
+    }
+    catch (IOException io){
+      System.err.println("errore lettura dati da tastiera");
+    }
   }
 }
